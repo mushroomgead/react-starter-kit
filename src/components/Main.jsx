8bb9caed5ae0c91500d76b916ff0204b8d9b849a
Gead <ke.adkead@hotmail.com>
2017-08-27 12:54:32 +0700

    add routers

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home';
 import Page1 from './Page1';
 import Page2 from './Page2';
 
 const Main = () => (
-  <Switch>
-    <Route path='/page1' component={Page1} />
-    <Route path='/page2' component={Page2} />
-  </Switch>
+  <Router>
+    <div>
+      <Route exact path='/' component={Home} />
+      <Route path='/page1' component={Page1} />
+      <Route path='/page2' component={Page2} />
+      <ul>
+        <li><Link to='page1' />Page1</li>
+        <li><Link to='page2' />Page2</li>
+      </ul>
+    </div>
+  </Router>
 )
 
 export default Main;
